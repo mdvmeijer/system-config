80405160d0d8a4da3ee8738bf30fa42d050dc523
mdvmeijer <mdvmeijer@protonmail.com>
2023-03-22 20:36:36 +0200

    refactor system packages to 'base-packages.nix'

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,7 @@ in
       (./. + "${modules}/python.nix")
       (./. + "${modules}/vscode.nix")
       (./. + "${modules}/mullvad-vpn.nix")
+      (./. + "${modules}/base-packages.nix")
     ];
 
   ######### Core system stuff #########
@@ -165,9 +166,10 @@ in
     home.file.".alacritty.yml".source = ./. + "${dotfiles}/.alacritty.yml";
   };
 
+  nixpkgs.config.allowUnfree = true;
+
   home-manager.users.${workUser} = { pkgs, ... }: {
     home.stateVersion = "22.11";
-    nixpkgs.config.allowUnfree = true;
 
     home.packages = with pkgs; [
       android-studio
@@ -187,91 +189,7 @@ in
   # environment.etc."inputrc".source = ./. + "/dotfiles/nixos-inputrc";
   # environment.etc."inputrc".source = /home/meeri/.system-config/dotfiles/nixos-inputrc;
 
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-    wget
-    git
-    firefox
-    kate
-    discord
-    nodejs
-    vlc
-    libreoffice-qt
-    openssl
-    ffmpeg
-    alacritty
-    kdeconnect
-    htop
-    glances
-    qbittorrent
-    dotnet-sdk # for dafny course
-    mlocate
-    wofi
-    chromium
-    unzip
-    powertop
-    easyeffects
-
-    # TODO: include in virtualization module
-    virt-manager
-
-    yakuake
-    # kamoso (using nix-env atm)
-    killall
-    google-chrome
-    lm_sensors
-    galaxy-buds-client
-    spotify
-    smartmontools
-    bat
-    obsidian
-    nicotine-plus
-    gimp
-    zola
-    lf
-    file
-    feh
-
-    # PDF utils
-    ocrmypdf  # Add OCR layer to PDF file
-    qpdf  # e.g. rotating, splitting, merging, encryption
-
-    tldr
-    zathura
-    libnotify
-    youtube-dl
-    signal-desktop
-    tmuxinator
-  ];
-
-  programs.dconf.enable = true;
-
-  programs.tmux = {
-    enable = true;
-    clock24 = true;
-  };
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      # discord package from nixpkgs is outdated: it prompts for an update and is otherwise unusable
-      discord = prev.discord.overrideAttrs (
-        _: { src = builtins.fetchTarball {
-          url =
-          "https://discord.com/api/download?platform=linux&format=tar.gz";
-          sha256 = "087p8z538cyfa9phd4nvzjrvx4s9952jz1azb2k8g6pggh1vxwm8";
-        }; }
-      );
-    })
-  ];
-
   ######### /Users & packages ##########
-
-
-  fonts.fonts = with pkgs; [
-    iosevka
-    agave
-  ];
 
   nix = {
     package = pkgs.nixFlakes;
---
+++ modules/base-packages.nix
@@ -0,0 +1,79 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+    firefox
+    kate
+    discord
+    nodejs
+    vlc
+    libreoffice-qt
+    openssl
+    ffmpeg
+    alacritty
+    kdeconnect
+    htop
+    glances
+    qbittorrent
+    dotnet-sdk # for dafny course
+    mlocate
+    wofi
+    chromium
+    unzip
+    powertop
+    easyeffects
+
+    # TODO: include in virtualization module
+    virt-manager
+
+    yakuake
+    # kamoso (using nix-env atm)
+    killall
+    google-chrome
+    lm_sensors
+    galaxy-buds-client
+    spotify
+    smartmontools
+    bat
+    obsidian
+    nicotine-plus
+    gimp
+    zola
+    lf
+    file
+    feh
+
+    # PDF utils
+    ocrmypdf  # Add OCR layer to PDF file
+    qpdf  # e.g. rotating, splitting, merging, encryption
+
+    tldr
+    zathura
+    libnotify
+    youtube-dl
+    signal-desktop
+    tmuxinator
+  ];
+
+  programs.dconf.enable = true;
+
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+  };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      # discord package from nixpkgs is outdated: it prompts for an update and is otherwise unusable
+      discord = prev.discord.overrideAttrs (
+        _: { src = builtins.fetchTarball {
+          url =
+          "https://discord.com/api/download?platform=linux&format=tar.gz";
+          sha256 = "087p8z538cyfa9phd4nvzjrvx4s9952jz1azb2k8g6pggh1vxwm8";
+        }; }
+      );
+    })
+  ];
+}
