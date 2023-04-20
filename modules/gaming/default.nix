ec25df70c51bebfc9005a8b04550aac968d68a3e
mdvmeijer <mdvmeijer@protonmail.com>
2023-04-20 10:50:50 +0300

    remove nixos-gaming stuff

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -10,18 +10,18 @@ in
     dedicatedServer.openFirewall = true;
   };
 
-  home-manager.users.${mainUser} = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      lutris
-      legendary-gl
-    ];
-  };
-
-  nix = {
-    # cachix for nixos-gaming
-    settings = {
-      substituters = [ "https://nix-gaming.cachix.org" ];
-      trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
-    };
-  };
+#   home-manager.users.${mainUser} = { pkgs, ... }: {
+#     home.packages = with pkgs; [
+#       lutris
+#       legendary-gl
+#     ];
+#   };
+# 
+#   nix = {
+#     # cachix for nixos-gaming
+#     settings = {
+#       substituters = [ "https://nix-gaming.cachix.org" ];
+#       trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
+#     };
+#   };
 }
