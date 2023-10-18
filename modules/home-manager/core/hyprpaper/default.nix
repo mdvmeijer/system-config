05dbe235aa6e7170b3cb97ec4699b2285a24288a
M.D.V. Meijer <mdvmeijer@protonmail.com>
2023-10-18 14:43:50 +0200

    Include hyprpaper config lines for all outputs

--- modules/home-manager/core/hyprpaper/default.nix
+++ modules/home-manager/core/hyprpaper/default.nix
@@ -9,8 +9,11 @@
       preload = ${wallpaper}
 
       wallpaper = eDP-1,${wallpaper}
+      wallpaper = DP-1,${wallpaper}
+      wallpaper = DP-2,${wallpaper}
       wallpaper = DP-3,${wallpaper}
       wallpaper = DP-4,${wallpaper}
+      wallpaper = HDMI-A-1,${wallpaper}
     '';
   };
 }
