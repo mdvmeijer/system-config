b9311a942990b590efb022f839fdafd170f85e6d
M.D.V. Meijer <mdvmeijer@protonmail.com>
2024-06-22 12:57:41 +0200

    gnuradio: Fix rtl-sdr config

--- modules/gnuradio.nix
+++ modules/gnuradio.nix
@@ -13,12 +13,11 @@
     rtl-sdr-osmocom
   ];
 
+  hardware.rtl-sdr.enable = true;
+
   # Without udev rules, gnuradio cannot access the rtl-sdr dongle.
   # See https://github.com/jopohl/urh/wiki/SDR-udev-rules
   services.udev.extraRules = ''
-    # original RTL2832U vid/pid (hama nano, for example)
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="2832", MODE:="0666"
-
     # limesuite rules
     # See https://github.com/myriadrf/LimeSuite/blob/master/udev-rules/64-limesuite.rules
     SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="8613", SYMLINK+="stream-%k", MODE="666"
